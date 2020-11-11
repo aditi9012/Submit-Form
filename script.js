dd15640e8b5b10eb48b462aba1adcfdfb47d1f3f
aditi9012 <aditi.1721it1011@kiet.edu>
2020-11-11 17:47:48 +0530

    added css

---
+++ script.js
@@ -0,0 +1,13 @@
+function formValidation(){
+    var firstName=document.registration.firstName;
+    var lastName=document.registration.lastName;
+    var email=document.registration.email;
+    var address=document.registration.address;
+    var pinCode=document.registration.pinCode;
+    var mobile=document.registration.mobile;
+    var dOB=document.registration.dOB;
+    
+    if(firstName.length()==0){
+       alert('hi');
+    }
+}
